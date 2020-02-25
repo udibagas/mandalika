6e02a23522af247e63fc056fbcbcfc533b9aa12f
Bagas Udi Sahsangka <udibagas@gmail.com>
2020-02-25 10:18:22 +0700

    simpen data bisa pake post & get

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@
 
 Route::post('login', 'AuthController@login');
 
-Route::group(['middleware' => 'auth:api'], function() {
+Route::group(['middleware' => 'auth:api'], function () {
     Route::post('artisan', 'AdminController@artisan');
-    Route::post('sensorLog', 'SensorLogController@store');
+    Route::match(['get', 'post'], 'sensorLog', 'SensorLogController@store');
 });
